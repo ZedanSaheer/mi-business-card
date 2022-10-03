786ee28288458998cf7178f883532e988d468978
Zedan Saheer <zed.saheer5@gmail.com>
2022-10-04 04:15:08 +0530

    Added padding , safe area. Understand more about layout

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    Main(),
+  );
+}
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            child: Text('Hello'),
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.symmetric(
+              vertical: 15,
+              horizontal: 5,
+            ),
+            padding: EdgeInsets.all(10),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
