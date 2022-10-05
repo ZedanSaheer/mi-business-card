d18f67b71239c9742df6cb3712a63cf4b498fcb0
Zedan Saheer <zed.saheer5@gmail.com>
2022-10-05 07:06:14 +0530

    Created a custom bussiness profile card

--- lib/main.dart
+++ lib/main.dart
@@ -13,26 +13,71 @@ class Main extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-              width: 30.0,
-              color: Colors.red,
-              child: Text('Container Two'),
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/zedan.png'),
             ),
-            SizedBox(
-              width: 20.0,
+            Text(
+              'Zedan Saheer',
+              style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontFamily: 'Poppins'),
             ),
-            Container(
-              width: 50.0,
-              color: Colors.blue,
-              child: Text('Container Two'),
+            Text(
+              'Software Engineer',
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'Poppins',
+                  color: Colors.white,
+                  fontStyle: FontStyle.normal),
             ),
-            Container(
-              width: 40.0,
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
               color: Colors.white,
-              child: Text('Container Two'),
+              child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    size: 30.0,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+966 12345678',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: 'Poppins',
+                      fontSize: 20.0,
+                    ),
+                  )),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 25.0,
+              ),
+              color: Colors.white,
+              child: ListTile(
+                leading: Icon(
+                  Icons.mail,
+                  size: 30.0,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'test@example.com',
+                  style: TextStyle(
+                    color: Colors.teal,
+                    fontFamily: 'Poppins',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
             ),
           ],
         )),
